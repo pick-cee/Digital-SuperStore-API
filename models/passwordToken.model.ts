dc90cf6d62b603aa791edde3c38b8c5eda272c13
pick-cee <akinloluwaolumuyide@gmail.com>
2022-08-28 00:45:20 +0100

    updates

--- models/passwordToken.model.ts
+++ models/passwordToken.model.ts
@@ -12,7 +12,7 @@ const passTokenSchema = new mongoose.Schema(
         },
         expiresIn: {
             type: mongoose.Schema.Types.Date,
-            default: new Date().setTime(new Date().getTime() + 114000)
+            default: new Date().setTime(new Date().getTime() + 1140000)
         },
     },
     { timestamps: true }
